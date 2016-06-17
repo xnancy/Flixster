dc75274a38096e0c4a711a3f35a622f6bff5d036
Nancy Xu <nancyxu@fb.com>
2016-06-17 15:09:44 -0700

    first commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Nancy Xu on 6/15/16.
+//  Copyright © 2016 Nancy Xu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flix/SearchTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableCell.swift
+//  Flix
+//
+//  Created by Nancy Xu on 6/16/16.
+//  Copyright © 2016 Nancy Xu. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
